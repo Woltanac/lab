3a5d67cb21654c88a39c3dfa986742fbde0f5e44
Woltanac <woltanac@gmail.com>
2025-11-09 22:17:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-// Use the port provided by Render's environment variable, or default to 3000 for local development
 const port = process.env.PORT || 3000;
 
-// Serve static files from the root directory
-app.use(express.static(path.join(__dirname)));
+// Serve static files from /public
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -14,4 +14,3 @@ app.get('/', (req, res) => {
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server listening on port ${port}`);
 });
-
